ac986872f4f89ed90861dcc86bcece48c372f8cd
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-11-13 00:03:17 +0200

    Add project files.

---
+++ FactorialProcessor/FactorialProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProcessor
+{
+    class FactorialProcessor
+    {
+        public void Go(int param, bool parallelMode)
+        {
+            if (param < 1 || param > 15)
+            {
+                throw new ArgumentOutOfRangeException("param", "Value must be between 1 and 15.");
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 1; i <= param; i++)
+            {
+                int n = i;
+
+                if (parallelMode)
+                {
+                    //creating a thread
+                    var thread = new Thread(() => 
+                    {
+                        int result = Factorial(n);
+                        Console.WriteLine($"Factorial of {n} = {result}");
+                    });
+                    //starting a thread
+                    thread.Start();
+                }
+                else
+                {
+                    int result = Factorial(n);
+                    Console.WriteLine($"Factorial of {n} = {result}");
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Execution time: {stopwatch.ElapsedTicks} ticks ({stopwatch.ElapsedMilliseconds} ms)");
+        }
+        private int Factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
---
+++ FactorialProcessor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FactorialProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorialProcessor processor = new FactorialProcessor();
+
+            Console.WriteLine("Parallel Mode:");
+            processor.Go(15, true);
+
+            Console.WriteLine("\nSequential Mode:");
+            processor.Go(10, false);
+        }
+    }
+}
